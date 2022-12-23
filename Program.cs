15c9bfe712074c7007050b1177ec3554d9253fef
Jennifer Massi <jennifer.massi@mscoding.org>
2022-12-23 11:27:19 -0800

    ASCII ART

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace ASCII_ART
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("= = = = = = = = = = = =          1C                  C1");
+            Console.WriteLine("         1                       1 C                C 1");
+            Console.WriteLine("         1                       1   C             C  1");
+            Console.WriteLine("         1                       1     C          C   1");
+            Console.WriteLine("         1                       1       C       C    1");
+            Console.WriteLine("         1                       1         C    C     1");
+            Console.WriteLine("         1                       1          cccc      1");
+            Console.WriteLine("         1                       1                    1");
+            Console.WriteLine(" c       1                       1                    1");
+            Console.WriteLine("   c     c                       1                    1");
+            Console.WriteLine("      c  c                       1                    1");
+
+            Console.WriteLine(" d[ O_O ]b");
+
+            Console.WriteLine(" ^")
+                
+        }
+    }
+}
